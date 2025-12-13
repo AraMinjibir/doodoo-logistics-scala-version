6b98dcc137c4dfcf59c1ace1a4f6ef4bb26f8921
AraMinjibir <abubakar.aramjb@gmail.com>
2025-12-13 12:02:08 +0100

    Cost calculator and Delivery date estimator implemented to maintain SRP

--- app/utilities/CostCalculator.scala
+++ app/utilities/CostCalculator.scala
@@ -1,5 +1,7 @@
 package utilities
 
-class CostCalculator {
+import domain.models.{PackageDetails, Recipient}
 
+trait CostCalculator {
+  def calculate(packageDetails: PackageDetails, recipient: Recipient): BigDecimal
 }
--- app/utilities/DateEstimator.scala
+++ app/utilities/DateEstimator.scala
@@ -1,5 +1,9 @@
 package utilities
 
-class DateEstimator {
+import domain.models.Recipient
 
+import java.time.Instant
+
+trait DateEstimator {
+  def estimate(recipient: Recipient): Option[Instant]
 }
