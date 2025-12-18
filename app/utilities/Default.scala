e2b52c0f39841281a9bb3abbefefb91828770ab1
AraMinjibir <abubakar.aramjb@gmail.com>
2025-12-18 13:45:36 +0100

    implement default utilities
    - Annotate ShipmentRowMapper as @Singleton for Guice dependency injection
    - Implement concrete logic for CostCalculator and DateEstimator in Default.scala

--- app/utilities/Default.scala
+++ app/utilities/Default.scala
@@ -1,5 +1,23 @@
 package utilities
 
-class Default {
+import com.google.inject.{Inject, Singleton}
+import domain.models.{PackageDetails, Recipient}
+
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
+@Singleton
+class Default @Inject() extends CostCalculator with DateEstimator {
+  override def calculate(packageDetails: PackageDetails, recipient: Recipient): BigDecimal = {
+    val weight = packageDetails.weight
+    val basePrice = BigDecimal(10.0)
+    val ratePerKg = BigDecimal(2.5)
+
+    basePrice + ((weight) * ratePerKg)
+  }
+
+  override def estimate(recipient: Recipient): Option[Instant] = {
+    Some(Instant.now().plus(3, ChronoUnit.DAYS))
+  }
 
 }
