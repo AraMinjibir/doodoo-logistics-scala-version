513988cc9c7b557a67f9153995bbda68ca6396f2
AraMinjibir <abubakar.aramjb@gmail.com>
2025-12-18 13:36:44 +0100

    refactor: implement Guice DI module for automatic dependency injection
    - Create Module.scala to bind Read and Write repositories to Slick implementations
    - Enable custom Module in application.conf
    - Decouple controllers and services from concrete repository implementations

--- app/Module.scala
+++ app/Module.scala
@@ -1,3 +1,30 @@
-class Module {
+import com.google.inject.AbstractModule
+import domain.services.ShipmentService
+import domain.services.impl.ShipmentServiceImpl
+import domain.validation.ShipmentValidation
+import domain.validation.impl.ShipmentValidationImpl
+import mappers.ShipmentRowMapper
+import repositories.read.{ShipmentReadRepository, SlickShipmentReadRepository}
+import repositories.write.{ShipmentWriteRepository, SlickShipmentWriteRepository}
+import utilities.{CostCalculator, DateEstimator, Default}
 
+class Module extends AbstractModule {
+  override def configure(): Unit = {
+    // Bind Repositories
+    bind(classOf[ShipmentReadRepository]).to(classOf[SlickShipmentReadRepository])
+    bind(classOf[ShipmentWriteRepository]).to(classOf[SlickShipmentWriteRepository])
+
+    // Bind Services
+    bind(classOf[ShipmentService]).to(classOf[ShipmentServiceImpl])
+//Bind Mapper
+
+    bind(classOf[ShipmentRowMapper]).asEagerSingleton()
+
+    // Bind Validation
+    bind(classOf[ShipmentValidation]).to(classOf[ShipmentValidationImpl])
+
+    // Bind Utilities
+    bind(classOf[CostCalculator]).to(classOf[Default])
+    bind(classOf[DateEstimator]).to(classOf[Default])
+  }
 }
