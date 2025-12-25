a295c5574f6e2db179e8d9b7c7a9d92acff77e56
AraMinjibir <abubakar.aramjb@gmail.com>
2025-12-25 19:00:17 +0100

    fix(di): bind UserService trait to implementation in Module
    - Added Guice binding for UserService to UserServiceImpl.
    - Resolved CreationException in E2E tests caused by missing DI implementation.
    - Standardized dependency wiring for the User management vertical slice.

--- app/Module.scala
+++ app/Module.scala
@@ -1,27 +1,33 @@
 import com.google.inject.AbstractModule
-import domain.services.ShipmentService
-import domain.services.impl.ShipmentServiceImpl
-import domain.validation.ShipmentValidation
-import domain.validation.impl.ShipmentValidationImpl
+import domain.services.{ShipmentService, UserService}
+import domain.services.impl.{ShipmentServiceImpl, UserServiceImpl}
+import domain.validation.{ShipmentValidation, UserValidation}
+import domain.validation.impl.{ShipmentValidationImpl, UserValidationImpl}
 import mappers.ShipmentRowMapper
-import repositories.read.{ShipmentReadRepository, SlickShipmentReadRepository}
-import repositories.write.{ShipmentWriteRepository, SlickShipmentWriteRepository}
+import repositories.read.{ShipmentReadRepository, SlickShipmentReadRepository, UserReadRepository, UserSlickReadRepository}
+import repositories.write.{ShipmentWriteRepository, SlickShipmentWriteRepository, SlickUserWriteRepository, UserWriteRepository}
 import utilities.{CostCalculator, DateEstimator, Default}
 
 class Module extends AbstractModule {
   override def configure(): Unit = {
+
     // Bind Repositories
     bind(classOf[ShipmentReadRepository]).to(classOf[SlickShipmentReadRepository])
     bind(classOf[ShipmentWriteRepository]).to(classOf[SlickShipmentWriteRepository])
+    bind(classOf[UserReadRepository]).to(classOf[UserSlickReadRepository])
+    bind(classOf[UserWriteRepository]).to(classOf[SlickUserWriteRepository])
 
     // Bind Services
     bind(classOf[ShipmentService]).to(classOf[ShipmentServiceImpl])
-//Bind Mapper
+    bind(classOf[UserService]).to(classOf[UserServiceImpl])
+
+  //Bind Mapper
 
     bind(classOf[ShipmentRowMapper]).asEagerSingleton()
 
     // Bind Validation
     bind(classOf[ShipmentValidation]).to(classOf[ShipmentValidationImpl])
+    bind(classOf[UserValidation]).to(classOf[UserValidationImpl])
 
     // Bind Utilities
     bind(classOf[CostCalculator]).to(classOf[Default])
