ecb3bc6fda3a2ce70c007870564fa1bba991a15e
AraMinjibir <abubakar.aramjb@gmail.com>
2026-03-11 11:15:44 +0100

    test(user): add integration tests for SlickUserRepository
    - verify user persistence
    - test user status update
    - test find by id, email, role, and status
    - test listing users
    - configure in-memory H2 database for repository tests

--- test/scala/domain/repository/SlickUserRepository.scala
+++
@@ -1,5 +0,0 @@
-package scala.domain.repository
-
-class SlickUserRepository {
-
-}
---
+++ test/scala/domain/repository/SlickUserRepositoryIT.scala
@@ -0,0 +1,165 @@
+package scala.domain.repository
+
+import infrastructure.persistence.tables.UserTable
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.inject.guice.GuiceApplicationBuilder
+import repositories.UserRepository
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, ExecutionContext}
+import domain.models.{User, UserRole, UserStatus}
+import domain.models.UserRole.{Admin, Recipient}
+import domain.models.UserStatus.Suspended
+
+class SlickUserRepositoryIT
+  extends AnyWordSpec
+    with Matchers
+    with GuiceOneAppPerSuite
+    with BeforeAndAfterAll
+    with BeforeAndAfterEach{
+
+  lazy val ec = app.injector.instanceOf[ExecutionContext]
+
+  override def fakeApplication() =
+    GuiceApplicationBuilder()
+      .configure(
+        "slick.dbs.default.profile" -> "slick.jdbc.H2Profile$",
+        "slick.dbs.default.db.driver" -> "org.h2.Driver",
+        "slick.dbs.default.db.url" -> "jdbc:h2:mem:test_write_repo;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+        "slick.dbs.default.db.user" -> "sa",
+        "slick.dbs.default.db.password" -> ""
+      ).build()
+
+  lazy val repo     = app.injector.instanceOf[UserRepository]
+
+  lazy val dbConfig = app.injector.instanceOf[DatabaseConfigProvider].get[JdbcProfile]
+  import dbConfig.profile.api._
+
+  override def beforeAll(): Unit = {
+    val setupAction = DBIO.seq(
+     UserTable.table.schema.dropIfExists,
+      UserTable.table.schema.create,
+    )
+    Await.result(dbConfig.db.run(setupAction), 5.seconds)
+  }
+
+  override def beforeEach(): Unit = {
+    Await.result(
+      dbConfig.db.run(
+        DBIO.seq(
+          UserTable.table.delete,
+        )), 5.seconds)
+  }
+
+  val newUser = User.createUser(
+    name = "DooDoo User",
+    email = "DooDooUser@gmail.com",
+    phone = "07022223456",
+    role = Admin
+  ).fold(
+    errors => throw new RuntimeException(errors.mkString(",")),
+    identity
+  )
+  val newUser2 = User.createUser(
+    name = "DooDoo User2",
+    email = "DooDooUser2@gmail.com",
+    phone = "07022223456",
+    role = Recipient
+  ).fold(
+    errors => throw new RuntimeException(errors.mkString(",")),
+    identity
+  )
+
+  "SlickUserRepository" should {
+    "Persist a new user row into the database" in {
+
+      val result =  Await.result(repo.createUser(newUser), 5.second)
+      result.isSuccess shouldBe true
+
+      val persisted = Await.result(dbConfig.db.run(UserTable.table.result), 5.second)
+
+      persisted should have size 1
+      persisted.head.status shouldBe newUser.status
+      persisted.head.id shouldBe newUser.id
+      persisted.head.role shouldBe newUser.role
+    }
+    "Successfully update user status to Suspended" in{
+      Await.result(repo.createUser(newUser), 5.second)
+
+      val newStatus = newUser.copy(
+        id = newUser.id,
+        name = newUser.name,
+        email = newUser.email,
+        hashPassword = newUser.hashPassword,
+        phone = newUser.phone,
+        role = newUser.role,
+        status = Suspended,
+        createdAt = newUser.createdAt,
+        updatedAt = newUser.updatedAt
+      )
+
+      val result = Await.result(repo.updateUserDetails(newStatus), 5.second)
+      result.isSuccess shouldBe true
+
+      val updatedRow = Await.result(dbConfig.db.run(UserTable.table.filter(_.id === newUser.id).result.head), 5.second)
+
+      updatedRow.status shouldBe UserStatus.Suspended
+    }
+    "Return a user with a given valid id" in {
+      Await.result(repo.createUser(newUser), 5.second)
+
+      val result = Await.result(repo.findUserById(newUser.id),5.second)
+
+      result.map(_.id) shouldBe Some(newUser.id)
+    }
+    "Return None when user id does not exist" in {
+      val randomId = java.util.UUID.randomUUID()
+
+      val result = Await.result(repo.findUserById(randomId), 5.seconds)
+
+      result shouldBe None
+    }
+    "Retrieve a user with a given Role" in{
+      Await.result(repo.createUser(newUser), 5.second)
+
+      val result = Await.result(repo.findUserByRole(newUser.role), 5.second)
+      result.head.role shouldBe UserRole.Admin
+    }
+    "Return a user with a given email " in{
+      Await.result(repo.createUser(newUser), 5.second)
+
+      val result = Await.result(repo.findUserByEmail(newUser.email), 5.second)
+      result.head.email shouldBe newUser.email
+
+    }
+    "Retrieve users by status" in {
+      Await.result(repo.createUser(newUser), 5.seconds)
+
+      val result = Await.result(repo.findUserByStatus(newUser.status), 5.seconds)
+
+      result should not be empty
+    }
+    "Return all users inserted" in{
+      val user1 = newUser
+      val user2 = newUser2
+
+      Await.result(repo.createUser(user1), 5.second)
+      Await.result(repo.createUser(user2), 5.second)
+
+      val result = Await.result(repo.listAllUsers, 5.second)
+
+      result should have size 2
+    }
+    "Return empty when no user found" in {
+      val result =  Await.result(repo.listAllUsers, 5.second)
+
+      result shouldBe Seq.empty
+    }
+  }
+
+}
