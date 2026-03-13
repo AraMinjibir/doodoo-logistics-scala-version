678d1d3873982c093207468948c9970c425f2bef
AraMinjibir <abubakar.aramjb@gmail.com>
2026-03-13 13:55:54 +0100

    feature(e2e and Json):Add JSON serializers and E2E tests for User management
    - Added  for Play JSON formatters for User, UserRole, and UserStatus.
    - Added  to cover full authentication lifecycle using TestContainers.
    - Ensures sign-up, login, and token generation are tested end-to-end against a real PostgreSQL container.

---
+++ test/scala/domain/e2e/UserE2ESpec.scala
@@ -0,0 +1,146 @@
+package scala.domain.e2e
+
+import com.dimafeng.testcontainers.{ForAllTestContainer, PostgreSQLContainer}
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json.{JsValue, Json}
+import play.api.libs.ws.WSClient
+
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+
+class UserE2ESpec extends PlaySpec
+  with GuiceOneServerPerSuite
+  with ForAllTestContainer{
+
+  override val container: PostgreSQLContainer = PostgreSQLContainer("postgres:16-alpine")
+
+  override def fakeApplication() = {
+    // These values are generated dynamically by Docker
+    new GuiceApplicationBuilder()
+      .configure(
+        "slick.dbs.default.profile" -> "slick.jdbc.PostgresProfile$",
+        "slick.dbs.default.db.driver" -> "org.postgresql.Driver",
+        "slick.dbs.default.db.url" -> container.jdbcUrl,
+        "slick.dbs.default.db.user" -> container.username,
+        "slick.dbs.default.db.password" -> container.password,
+        "play.evolutions.db.default.enabled" -> "true",
+        "play.evolutions.db.default.autoApply" -> "true",
+
+        //        connection timeout for failing
+        "slick.dbs.default.db.numThreads" -> "5",
+        "slick.dbs.default.db.maxConnections" -> "5",
+        "slick.dbs.default.db.connectionTimeout" -> "5000", // 5 seconds
+        "slick.dbs.default.db.registerMbeans" -> "true",
+
+        "play.http.errorHandler" -> "play.api.http.DefaultHttpErrorHandler",
+
+        //        Debugger
+        "logger.slick.jdbc.JdbcBackend.statement" -> "DEBUG",
+        "logger.com.zaxxer.hikari" -> "DEBUG"
+      )
+      .build()
+
+  }
+    "Auth System E2E" should {
+
+      "handle full authentication lifecycle" in {
+
+        val wsClient = app.injector.instanceOf[WSClient]
+        val baseUrl = s"http://localhost:$port/users"
+
+        // SIGN UP
+
+        val signUpPayload = Json.obj(
+          "name" -> "DooDoo User",
+          "email" -> "doodoo@gmail.com",
+          "password" -> "strongPassword123",
+          "phone" -> "07022223456",
+          "role" -> "Admin"
+        )
+
+        val signUpResponse = Await.result(
+          wsClient.url(s"$baseUrl/signUp")
+            .addHttpHeaders("Content-Type" -> "application/json")
+            .post(signUpPayload),
+          10.seconds
+        )
+
+        signUpResponse.status mustBe 201
+
+        val signUpJson: JsValue = signUpResponse.json
+        val userId = (signUpJson \ "id").as[String]
+        val token = (signUpJson \ "token").as[String]
+
+        // 2 LOGIN
+
+        val loginPayload = Json.obj(
+          "email" -> "doodoo@gmail.com",
+          "hashPassword" -> "strongPassword123"
+        )
+
+        val loginResponse = Await.result(
+          wsClient.url(s"$baseUrl/login")
+            .addHttpHeaders("Content-Type" -> "application/json")
+            .post(loginPayload),
+          10.seconds
+        )
+
+        loginResponse.status mustBe 200
+
+        val loginJson = loginResponse.json
+        val jwt = (loginJson \ "token").as[String]
+
+        jwt must not be empty
+
+        //  GET USER (Protected)
+
+        val getUserResponse = Await.result(
+          wsClient.url(s"$baseUrl/id/$userId")
+            .addHttpHeaders("Authorization" -> s"Bearer $jwt")
+            .get(),
+          10.seconds
+        )
+
+
+        getUserResponse.status mustBe 200
+
+        // 4️ UPDATE USER
+
+        val updatePayload = Json.obj(
+          "name" -> "Updated User",
+          "email" -> "doodoo@gmail.com",
+          "password" -> "strongPassword123",
+          "phone" -> "07000000000",
+          "role" -> "Admin"
+        )
+
+        val updateResponse = Await.result(
+          wsClient.url(s"$baseUrl/update/$userId")
+            .addHttpHeaders(
+              "Content-Type" -> "application/json",
+              "Authorization" -> s"Bearer $jwt"
+            )
+            .put(updatePayload),
+          10.seconds
+        )
+
+        println(updateResponse.body)
+        updateResponse.status mustBe 200
+
+        // 5️ DELETE USER
+
+        val deleteResponse = Await.result(
+          wsClient.url(s"$baseUrl/$userId")
+            .addHttpHeaders("Authorization" -> s"Bearer $jwt")
+            .delete(),
+          10.seconds
+        )
+
+        deleteResponse.status mustBe 204
+      }
+    }
+
+
+}
