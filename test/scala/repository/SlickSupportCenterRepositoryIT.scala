63ec022fccedf1b9663644957ac927abaf73654f
AraMinjibir <146440999+AraMinjibir@users.noreply.github.com>
2026-06-13 20:35:27 +0100

    Remove unused imports

--- test/scala/repository/SlickSupportCenterRepositoryIT.scala
+++ test/scala/repository/SlickSupportCenterRepositoryIT.scala
@@ -13,7 +13,6 @@ import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.inject.guice.GuiceApplicationBuilder
 import slick.jdbc.JdbcProfile
-import slick.dbio.DBIO
 import play.api.db.slick.DatabaseConfigProvider
 
 import scala.concurrent.duration.DurationInt
